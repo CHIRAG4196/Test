1ca0b6697533fb7f5bdf8c188129f1acffd61cf2
utsavshah95 <utsavshah95@yahoo.in>
2017-04-13 18:01:25 +0530

    init

---
+++ app/src/main/java/com/example/utsav/test/activity/RecycleActivity.java
@@ -0,0 +1,15 @@
+package com.example.utsav.test.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.utsav.test.R;
+
+public class RecycleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle);
+    }
+}
